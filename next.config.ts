a5abc8dee0b1657d2d5aa37f15a1df473a577723
Manoj Kumar R <zztmmano@Eichergroup.com>
2024-12-05 16:38:27 +0530

    next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    customKey : process.env.NEXT_PUBLIC_CUSTOM_KEY
+  }
 };
 
 export default nextConfig;
